51733af67965c353d5f28591aa896ac51a81979b
Shoma Fujino <gori79ty@gmail.com>
2025-10-03 08:39:01 +0900

    feat: add P005_ReverseNumbers in dojo/2025

---
+++ dojo/2025/P005_ReverseNumbers.java
@@ -0,0 +1,9 @@
+// P005: 1〜10を逆順に表示する
+
+public class P005_ReverseNumbers {
+  public static void main(String[] args) {
+    for (int i = 10; i >= 1; i-- ) {
+      System.out.println(i);
+    }
+  }
+}
